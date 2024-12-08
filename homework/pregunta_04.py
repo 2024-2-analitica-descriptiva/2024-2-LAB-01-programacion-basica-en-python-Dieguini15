ed55b8ad7f1e7829a4a2dc675ae7866343d16412
Dieguini15 <jdquinteroc@unal.edu.co>
2024-12-07 21:59:41 -0500

    Lab 1 nuevo intento

--- homework/pregunta_04.py
+++ homework/pregunta_04.py
@@ -26,22 +26,3 @@ def pregunta_04():
     resultados = list(zip(meses, conteos))
     return resultados
 print(pregunta_04())
-"""
-La columna 3 contiene una fecha en formato `YYYY-MM-DD`. Retorne la
-cantidad de registros por cada mes, tal como se muestra a continuación.
-
-Rta/
-[('01', 3),
-    ('02', 4),
-    ('03', 2),
-    ('04', 4),
-    ('05', 3),
-    ('06', 3),
-    ('07', 5),
-    ('08', 6),
-    ('09', 3),
-    ('10', 2),
-    ('11', 2),
-    ('12', 3)]
-
-"""
